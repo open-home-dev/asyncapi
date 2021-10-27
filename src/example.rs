72527deadb7a979bc54f0ff745562435d8b00750
JP Stauffert <jp@unbugged.org>
2021-10-27 07:25:09 +0200

    Changed the example struct to closer resemble the potential future spec

--- src/example.rs
+++ src/example.rs
@@ -1,29 +1,30 @@
 use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
 
-// TODO copied from openapi but doesn't exist in the asyncapi spec
+/// The asyncapi spec doesn't describe a structured example object.
+///
+/// This library, however, tries to serialize examples into
+/// this struct for easier handling.
+/// See [issue #606](https://github.com/asyncapi/spec/issues/606) for a
+/// proposal
 #[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq)]
 pub struct Example {
-    /// Short description for the example.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    /// A machine-friendly name.
+    pub name: Option<String>,
+    /// A short summary of what the example is about.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub summary: Option<String>,
     /// Long description for the example.
     /// CommonMark syntax MAY be used for rich text representation.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
-    /// Embedded literal example. The `value` field and `externalValue`
-    /// field are mutually exclusive. To represent examples of
-    /// media types that cannot naturally represented in JSON or YAML,
-    /// use a string value to contain the example, escaping where necessary.
+    /// Payload as described in the `websocket-gemini` example.
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub value: Option<serde_json::Value>,
-    /// A URL that points to the literal example.
-    /// This provides the capability to reference examples that cannot
-    /// easily be included in JSON or YAML documents. The `value` field and
-    /// `externalValue` field are mutually exclusive.
-    #[serde(rename = "externalValue")]
+    pub payload: Option<serde_json::Value>,
+    /// field name proposed in the [issue #606](https://github.com/asyncapi/spec/issues/606)
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub external_value: Option<String>,
+    pub headers: Option<serde_json::Value>,
     /// Inline extensions to this object.
     #[serde(flatten)]
     pub extensions: IndexMap<String, serde_json::Value>,
