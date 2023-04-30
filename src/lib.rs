69eb560000176d730273dbf092c213f2fe19bff4
Srylax <a5910f11@srylax.dev>
2023-04-30 10:46:32 +0200

    First Draft

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod components;
 mod correlation_id;
 mod example;
 mod external_documentation;
+#[cfg(not(feature = "openapi-info"))]
 mod info;
 mod message;
 pub mod message_binding;
@@ -12,8 +13,17 @@ mod message_trait;
 pub mod operation_binding;
 mod operation_trait;
 mod parameter;
+#[cfg(not(feature = "openapi-schema"))]
 mod reference;
+#[cfg(not(feature = "openapi-schema"))]
 pub mod schema;
+#[cfg(feature = "openapi-schema")]
+pub mod schema {
+    pub use openapiv3::{
+        AdditionalProperties, AnySchema, ArrayType, IntegerFormat, IntegerType, NumberFormat,
+        NumberType, ObjectType, Schema, SchemaData, SchemaKind, StringFormat, StringType, Type,
+    };
+}
 mod security_scheme;
 mod server;
 pub mod server_binding;
@@ -21,19 +31,25 @@ mod tag;
 mod variant_or;
 
 pub use api::AsyncAPI;
-pub use channel::{Channel, Operation};
+pub use channel::{Channel, Operation, OperationMessageType};
 pub use channel_binding::ChannelBinding;
 pub use components::Components;
 pub use correlation_id::CorrelationId;
 pub use example::Example;
 pub use external_documentation::ExternalDocumentation;
+#[cfg(not(feature = "openapi-info"))]
 pub use info::{Contact, Info, License};
 pub use message::Message;
 pub use message_binding::MessageBinding;
 pub use message_trait::MessageTrait;
+#[cfg(feature = "openapi-schema")]
+pub use openapiv3::ReferenceOr;
+#[cfg(feature = "openapi-info")]
+pub use openapiv3::{Contact, Info, License};
 pub use operation_binding::OperationBinding;
 pub use operation_trait::OperationTrait;
 pub use parameter::Parameter;
+#[cfg(not(feature = "openapi-schema"))]
 pub use reference::ReferenceOr;
 pub use schema::Schema;
 pub use security_scheme::SecurityScheme;
